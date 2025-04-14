59bf917ecac9fcf3a8e2eea545056424398818fe
DasunNirmal <dasun200301@gmail.com>
2025-04-14 22:18:58 +0530

    Wrapped the sidebar component to allow pass the open close state to all the components

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,15 +1,18 @@
 import {Navigation} from "./Navigation.tsx";
 import {Outlet, useLocation} from "react-router";
+import {useState} from "react";
+import {SidebarContext} from "./SIdebarContext.tsx";
 
 export const RootLayout = () => {
 
     const {pathname} = useLocation();
     const showNavigation = pathname !== '';
+    const [open, setOpen] = useState(false)
 
     return (
-        <div>
-            {showNavigation && <Navigation/>}
-            <Outlet/>
-        </div>
+        <SidebarContext.Provider value={{open: open}}>
+            {showNavigation && <Navigation open={open} setOpen={setOpen} />}
+            <Outlet />
+        </SidebarContext.Provider>
     );
 }
