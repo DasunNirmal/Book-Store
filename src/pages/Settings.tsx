787e33171fe3a66d85c2de7aa1cbb19de39457a4
DasunNirmal <dasun200301@gmail.com>
2025-04-14 22:19:25 +0530

    animated

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,7 +1,18 @@
+import {motion} from "framer-motion";
+import {useSidebar} from "../components/SIdebarContext.tsx";
+
 export const Settings = () => {
+
+    const { open } = useSidebar();
+    const sidebarWidth = open ? 240 : 80;
+
     return (
-        <div>
+        <motion.div
+            initial={{ width: `calc(100% - ${sidebarWidth}px)` }}
+            animate={{ width: `calc(100% - ${sidebarWidth}px)` }}
+            transition={{ type: "spring", stiffness: 100, delay: open ? 0 : 0.5 }}
+            className='h-[900px] absolute top-0 right-0 p-8 border-2 border-amber-600'>
             <h1>Settings</h1>
-        </div>
+        </motion.div>
     );
 }
