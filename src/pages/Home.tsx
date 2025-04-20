c2a6110dd3012c27063027a57955b406591e6685
DasunNirmal <dasun200301@gmail.com>
2025-04-20 14:11:56 +0530

    book png

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,12 +7,24 @@ export const Home = () => {
     const sidebarWidth = open ? 240 : 80;
 
     return (
-        <motion.div
-            initial={{ width: `calc(100% - ${sidebarWidth}px)` }}
-            animate={{ width: `calc(100% - ${sidebarWidth}px)` }}
-            transition={{ type: "spring", stiffness: 100, delay: open ? 0 : 0.5 }}
-            className='h-[900px] absolute top-0 right-0 p-8 border-2 border-amber-600'>
-            <h1>Home</h1>
-        </motion.div>
+        <>
+            <motion.div
+                initial={{ width: `calc(100% - ${sidebarWidth}px)` }}
+                animate={{ width: `calc(100% - ${sidebarWidth}px)` }}
+                transition={{ type: "spring", stiffness: 100, delay: open ? 0 : 0.5 }}
+                className='h-[900px] absolute top-0 right-0 p-8 border-2 border-amber-600'>
+                <h1>Home</h1>
+                <div className="bg-[url('assets/1.png')] bg-no-repeat bg-center bg-cover h-[275px] w-[278px]"></div>
+                {/*<div className="bg-[url('assets/2.png')] bg-no-repeat bg-center bg-cover h-[250px] w-[1000px]"></div>*/}
+
+            </motion.div>
+            {/*<motion.div
+                initial={{ width: `calc(100% - ${sidebarWidth}px)` }}
+                animate={{ width: `calc(100% - ${sidebarWidth}px)` }}
+                transition={{ type: "spring", stiffness: 100, delay: open ? 0 : 0.5 }}
+                className='h-[900px] absolute top-[928px] right-0 p-8 border-2 border-amber-600'>
+                <h1>Second Home</h1>
+            </motion.div>*/}
+        </>
     );
 }
